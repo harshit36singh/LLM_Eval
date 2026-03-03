639d33b7633f0f05102c5dd35b4cd3d0ac0566d1
harshit36singh <singh36harshit@gmail.com>
2026-03-03 19:33:01 +0530

    added prompt entity

--- Backend/project/src/main/java/com/example/LLM_Eval/ProjectApplication.java
+++ Backend/project/src/main/java/com/example/LLM_Eval/ProjectApplication.java
@@ -1,4 +1,4 @@
-package com.example.project;
+package com.example.LLM_Eval;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ Backend/project/src/main/java/com/example/LLM_Eval/model/Prompt.java
@@ -0,0 +1,24 @@
+package com.example.LLM_Eval.model;
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Prompt {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,length = 2000)
+    private String text;
+
+    
+    private Instant createdAt=Instant.now();
+
+}
